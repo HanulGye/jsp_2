48ecb622d89cc401cd05f855f5eb2a17bf7d74fd
HanulGye <kyehl@naver.com>
2018-10-19 15:41:10 +0900

    Update src/com/hanul/notice/NoticeDTO.java
    create DTO

---
+++ src/com/hanul/notice/NoticeDTO.java
@@ -0,0 +1,12 @@
+package com.hanul.notice;
+
+import java.sql.Date;
+
+public class NoticeDTO {
+	//no(8), subject(400), content, writer(400), reg_date, hit(8)
+	
+	private int no, hit;
+	private String subject, content, writer;
+	private Date reg_date;
+	
+}
